2765285bc64b9d0f82ea9b022bb54acfb0c40cb3
Matt Diebolt <mdiebolt@gmail.com>
2011-07-03 21:55:15 -0700

    adding conditional switch to basic stylesheet

--- codium-extend/header.php
+++ codium-extend/header.php
@@ -4,7 +4,13 @@
 <meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 <meta name="viewport" content="width=device-width" />
 <title><?php bloginfo('name'); wp_title();?></title>
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+<?php
+  if (isset($_GET["basic"])) {
+?>
+    <link rel="stylesheet" type="text/css" media="all" href="http://blog.pixieengine.com/wp-content/themes/sprites-and-spells/basic.css" />
+<?php  } else { ?>
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<?php } ?>
 <link href='http://fonts.googleapis.com/css?family=PT+Sans&subset=latin' rel='stylesheet' type='text/css'>
 <link href="http://fonts.googleapis.com/css?family=Josefin+Sans:light,regular,600,bold" rel="stylesheet" type="text/css">
 <link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
@@ -17,24 +23,23 @@
 <body class="<?php codium_extend_body_class() ?>">
 
 <div id="wrapperpub">
-	<div id="header">
-		<div class="dp100">	
-			<h1 id="blog-title" class="blogtitle"><a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo('name') ?>"><?php bloginfo('name') ?></a></h1>
-			<div class="description"><?php bloginfo('description'); ?> </div>
-		</div><!-- dp100 -->		
-	</div><!--  #header -->	
-</div><!--  #wrapperpub -->			
+  <div id="header">
+    <div class="dp100">
+      <h1 id="blog-title" class="blogtitle"><a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo('name') ?>"><?php bloginfo('name') ?></a></h1>
+      <div class="description"><?php bloginfo('description'); ?> </div>
+    </div><!-- dp100 -->
+  </div><!--  #header -->
+</div><!--  #wrapperpub -->
 <div class="clear"></div>
-<div id="wrapper">	
-		<div id="access">
-			<div class="menu">
-				<ul>
-					<li>
-						<a href="<?php echo home_url( '/' ); ?>" title="home" class="top_parent"><?php printf(__('HOME', 'codium_extend')) ?></a>
-					</li>
-				</ul>
-			</div>
-					<?php wp_nav_menu(array( 'container_class' => 'menu-header', 'theme_location' => 'primary-menu',)); ?>			
-		</div><!--  #access -->	
-<div class="clear"></div>		
-	
+<div id="wrapper">
+    <div id="access">
+      <div class="menu">
+        <ul>
+          <li>
+            <a href="<?php echo home_url( '/' ); ?>" title="home" class="top_parent"><?php printf(__('HOME', 'codium_extend')) ?></a>
+          </li>
+        </ul>
+      </div>
+          <?php wp_nav_menu(array( 'container_class' => 'menu-header', 'theme_location' => 'primary-menu',)); ?>
+    </div><!--  #access -->
+<div class="clear"></div>
